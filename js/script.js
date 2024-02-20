68242d629f683a4b1872601382b6146a36fd0816
Martyna MH <martyna.michalak@gmail.com>
2024-02-20 12:05:28 +0100

    homework corrections

--- js/script.js
+++ js/script.js
@@ -1,45 +1,50 @@
 {
- const initHomePage = () => { 
-
   const welcome = () => {
     console.log("Łapa!");
   }
-  welcome();
-  
+
   const buttonChangeBackground = document.querySelector(".section__button");
-  const whatAmIDoing = document.querySelector(".section__whatAmIDoing");
-  const body = document.querySelector("body");
-  
-  let timeOfDay = "popołudnie";
 
   const toggleBackground = () => {
+    const body = document.querySelector("body");
+    
     body.classList.toggle("body--bisquitBackground");
 
     buttonChangeBackground.innerText = body.classList.contains(
       "body--bisquitBackground"
-     )
-    ? "Zmień kolor tła na białe"
-    : "Zmień kolor tła na ciasteczkowe";
+      )
+      ? "Zmień kolor tła na białe"
+      : "Zmień kolor tła na ciasteczkowe";
   }
 
-  buttonChangeBackground.addEventListener("click", toggleBackground);
-
   const setActivityBasedOnTimeOfDay = (time) => {
+    const whatAmIDoing = document.querySelector(".section__whatAmIDoing");
+    
     switch (time) {
-     case "wczesne rano":
+      case "wczesne rano":
       case "popołudnie":
-       whatAmIDoing.innerText = "spaceruję";
-       break;
-     case "rano":
+        whatAmIDoing.innerText = "spaceruję";
+        break;
+
+      case "rano":
       case "wieczór":
         whatAmIDoing.innerText = "jem";
-       break;
-     default:
-       whatAmIDoing.innerText = "śpię";
+        break;
+
+      default:
+        whatAmIDoing.innerText = "śpię";
     }
   };
-
+          
+  const initHomePage = () => { 
+            
+  welcome();
+            
+  buttonChangeBackground.addEventListener("click", toggleBackground);
+            
+  let timeOfDay = "popołudnie";
   setActivityBasedOnTimeOfDay(timeOfDay);
-  };
- initHomePage();
+};
+
+initHomePage();
 }
